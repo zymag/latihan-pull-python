156feed7c688f2acc47e60258a59ae76e945d8fd
Rayhan Rusyd <antianobl@gmail.com>
2021-02-17 20:58:21 +0700

    Create program.py

---
+++ program.py
@@ -0,0 +1,16 @@
+class BioData:
+  def __init__(self, nama, kelas):
+    self.nama = nama
+    self.kelas = kelas
+    self.umur = umur
+    self.agama = agama
+   
+   def printNama(self):
+    print("Nama anda: {self.nama}")
+    
+   def printKelas():
+    print("Kelas anda: {kelas}")
+   
+Ahmad = BioData("Ahmad Borat", "12 IPA 1", "18 Tahun", "Islam")
+Ahmad.printNama()
+Ahmad.printKelas
